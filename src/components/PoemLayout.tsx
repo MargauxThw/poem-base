80cce79d3fd02192b905a1633041dd4ae6d18dad
MargauxThw <36945000+MargauxThw@users.noreply.github.com>
2025-05-22 11:18:55 +1000

    PoemLayout styling and authors setup

--- src/components/PoemLayout.tsx
+++ src/components/PoemLayout.tsx
@@ -1,35 +1,39 @@
-import { useEffect, useState } from "react"
-import type { Poem } from "../utils/types"
-import Markdown from "react-markdown"
-import rehypeRaw from "rehype-raw"
-import { samplePoem } from "../utils/staticData"
+import { useEffect, useState } from 'react';
+import type { Poem } from '../utils/types';
+import Markdown from 'react-markdown';
+import rehypeRaw from 'rehype-raw';
+import { samplePoem } from '../utils/staticData';
+import { Separator } from './ui/separator';
+import { Link } from 'react-router-dom';
 
 export default function PoemLayout({ poem }: { poem: Poem | null }) {
-	const [poemState, setPoemState] = useState<Poem>(poem ? poem : (samplePoem))
+    const [poemState, setPoemState] = useState<Poem>(poem ? poem : samplePoem);
 
-	useEffect(() => {
-		window.scrollTo({
-			top: 0,
-			behavior: "smooth"
-		})
-		setPoemState(poem ? poem : samplePoem)
-	}, [poem])
+    useEffect(() => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+        setPoemState(poem ? poem : samplePoem);
+    }, [poem]);
 
-	return (
-		<>
-			<div>
-				<h2>{poemState.title}</h2>
-				<h3>{poemState.author}</h3>
-			</div>
+    return (
+        <>
+            <div className="flex flex-col items-start sm:items-start gap-4">
+                <h2 className="decoration-black font-bold text-xl">{poemState.title}</h2>
+                <Link to={`/authors/${poemState.author}`}>
+                    <h3 className="italic text-lg hover:underline">{poemState.author}</h3>
+                </Link>
+            </div>
 
-			<hr />
-			<section>
-				{poemState.lines.map((str, index) => (
-					<Markdown rehypePlugins={[rehypeRaw]} key={index}>
-						{`${str.trimStart()}<br/>`}
-					</Markdown>
-				))}
-			</section>
-		</>
-	)
+            <Separator />
+            <section>
+                {poemState.lines.map((str, index) => (
+                    <Markdown rehypePlugins={[rehypeRaw]} key={index}>
+                        {`${str.trimStart()}<br/>`}
+                    </Markdown>
+                ))}
+            </section>
+        </>
+    );
 }
