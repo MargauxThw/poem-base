7a512c8b3f69081c64dbfe12ff72564611f62c42
MargauxThw <36945000+MargauxThw@users.noreply.github.com>
2025-05-25 17:12:18 +1000

    Remove messagingId - not used

--- src/utils/firebaseConfig.ts
+++ src/utils/firebaseConfig.ts
@@ -1,18 +1,17 @@
-import { initializeApp } from "firebase/app"
-import { getAuth } from "firebase/auth"
-import { getFirestore } from "firebase/firestore"
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-	authDomain: "poem-base.firebaseapp.com",
-	projectId: "poem-base",
-	storageBucket: "poem-base.firebasestorage.app",
-	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-	appId: import.meta.env.VITE_FIREBASE_APP_ID
-}
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: 'poem-base.firebaseapp.com',
+    projectId: 'poem-base',
+    storageBucket: 'poem-base.firebasestorage.app',
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
 
-const app = initializeApp(firebaseConfig)
-const auth = getAuth(app)
-const db = getFirestore(app)
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-export { auth, db }
+export { auth, db };
